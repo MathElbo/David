ea554607d106e7c86359a0ebe178a0c13dcefbf1
MathElbo <mathijs.mg@hotmail.com>
2021-10-28 11:41:50 +0200

    base version

---
+++ layout-content/homepage.php
@@ -0,0 +1,8 @@
+<div class="jumbotron jumbotron-fluid homeJumbo">
+    <div class="container">
+        <h1 class="display-4">david.org</h1>
+        <p class="lead">David doet dingen</p>
+        <br>
+        <p>Kijk hier om eerdere projecten te zien</p>
+    </div>
+</div>

8fed2860ed98278ec0b1ad99d9656c3e806acb11
MathElbo <mathijs.mg@hotmail.com>
2022-01-23 12:38:14 +0100

    klusbedrijf ermelo

--- layout-content/homepage.php
+++ layout-content/homepage.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jumbotron-fluid homeJumbo">
     <div class="container">
-        <h1 class="display-4">david.org</h1>
-        <p class="lead">David doet dingen</p>
+        <h1 class="display-4">Klus bedrijf Ermelo</h1>
+        <p class="lead">Bij klus bedrijf ermelo doen we van alles wat</p>
         <br>
         <p>Kijk hier om eerdere projecten te zien</p>
     </div>
